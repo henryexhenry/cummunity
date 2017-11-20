40bc271af90d7e1d509b738af5e77fe568dc30c5
henryexhenry <921304158@qq.com>
2017-11-20 14:50:11 +0800

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,31 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from trips.views import home, post_detail, welcome, create, post_edit, register, login, logout
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^home/', home, name='home'),
+    url(r'^post/(?P<pk>\d+)/$', post_detail, name='post_detail'),
+    url(r'^$', welcome),
+    url(r'^create/$', create),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', post_edit, name='post_edit'),
+    url(r'^accounts/login/$', login),
+    url(r'^accounts/register/$', register),
+    url(r'^accounts/logout/', logout),
+]
